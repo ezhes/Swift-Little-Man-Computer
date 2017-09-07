7175bd5a8f7a79fb40d408735e19de726bd48242
Github <shusain93@gmail.com>
2017-09-06 20:38:00 -0600

    Initial Commit

---
+++ LMC/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LMC
+//
+//  Created by Salman Husain on 9/6/17.
+//  Copyright © 2017 Salman Husain. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LMC/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LMC
+//
+//  Created by Salman Husain on 9/6/17.
+//  Copyright © 2017 Salman Husain. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
